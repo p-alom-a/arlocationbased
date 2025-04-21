ede95d53d19c14c2d59f00def5f2f5168568b97b
Paloma <paloma.sanchezc@icloud.com>
2025-04-21 20:19:26 +0200

    Votre message de commit

--- src/main.js
+++ src/main.js
@@ -1,21 +1,22 @@
 import * as THREE from 'three';
 import * as LocAR from 'locar';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.001, 100);
+const camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.001, 1000);
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-window.addEventListener("resize", e => {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;    
-    camera.updateProjectionMatrix();
-});
-const box = new THREE.BoxGeometry(2,2,2);
-const cube = new THREE.Mesh(box, new THREE.MeshBasicMaterial({ color: 0xff0000 }));
+const scene = new THREE.Scene();
 
 const locar = new LocAR.LocationBased(scene, camera);
+
+window.addEventListener("resize", e => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+});
+
 const cam = new LocAR.Webcam({
     idealWidth: 1024,
     idealHeight: 768,
@@ -24,13 +25,35 @@ const cam = new LocAR.Webcam({
     }
 });
 
-locar.startGps(function() {
-    locar.add(cube, 1.4795619094617978, 44.11273960461151);
-  });
+let firstLocation = true;
+
+const deviceOrientationControls = new LocAR.DeviceOrientationControls(camera);
+
+locar.on("gpsupdate", (pos, distMoved) => {
+    if(firstLocation) {
+        // Coordonnées spécifiques pour le cube
+        const targetLongitude = 1.4795619094617978;
+        const targetLatitude = 44.11273960461151;
+
+        const geom = new THREE.BoxGeometry(10, 10, 10);
+        const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+        const mesh = new THREE.Mesh(geom, material);
+
+        locar.add(
+            mesh,
+            targetLongitude,
+            targetLatitude
+        );
+
+        firstLocation = false;
+    }
+});
+
+locar.startGps();
 
 renderer.setAnimationLoop(animate);
 
-
 function animate() {
+    deviceOrientationControls.update();
     renderer.render(scene, camera);
 }
